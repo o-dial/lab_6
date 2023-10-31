3f8e0ac6cb149c1f13e4b40afcc1e83ac0bef170
varunyelchur <varun.yelchur@ufl.edu>
2023-10-31 15:50:07 -0400

    added decoder

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
+#menu
+def menu():
+    # Printing the main menu:
+    print('Menu')
+    print('------------')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit')
 
+#encoder that adds three to each digit
+def encoder(password):
+    new_password = []
+    for char in str(password):
+        new_password.append(int(char) + 3)
+    return new_password
+
+#decoder that varun yelchur added
+def decoder(password):
+    final_list = []
+    temp_list = list(password.strip(" "))
+    for i in temp_list:
+        i = int(i)
+        i -= 3
+        i = str(i)
+        final_list.append(i)
+    password = str(''.join(final_list))
+    return password
+
+#main method
 if __name__ == '__main__':
     choice = 0
     while choice != 3:
