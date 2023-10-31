2d83729e90c5fdf9c43077f04f819252c6f8704c
o-dial <148977584+o-dial@users.noreply.github.com>
2023-10-31 10:49:29 -0400

    This is my main function.

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print('Hello world')
 
+if __name__ == '__main__':
+    choice = 0
+    while choice != 3:
+        menu()
+        choice = input('Please enter an option: ')
+        if choice == '1':
+            user_pass = input('Please enter your password to encode: ')
+            print(encoder(user_pass))
+            print('Your password has been encoded and stored.')
+        elif choice == '2':
+            new_pass = input('Please enter your password to decode: ')
+            print(decoder(new_pass))
+        elif choice == '3':
+            exit()
+        else:
+            print('Invalid choice, please try again.')
